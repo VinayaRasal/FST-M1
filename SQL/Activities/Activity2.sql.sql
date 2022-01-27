121f7632f6ad5cb3bc7dfba39b5b7ae5e3e31fe6
VinayaRasal <97938692+VinayaRasal@users.noreply.github.com>
2022-01-27 13:01:46 +0530

    Add files via upload

---
+++ SQL/Activities/Activity2.sql.sql
@@ -0,0 +1,20 @@
+REM   Script: Activity2
+REM   FST_BS22
+
+Select * from salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+insert all  
+into salesman values (5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+    select 1 from dual;
+
+Select * from salesman;
+
+
+Select * from salesman;
+
+
